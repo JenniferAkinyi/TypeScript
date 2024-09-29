87f52de620e057d2ce6cc9e445199d1f3e7e799c
Jennifer Akinyi <95017424+JenniferAkinyi@users.noreply.github.com>
2024-09-29 17:05:37 +0300

    Delete Basic_Annotations directory

--- Basic_Annotations/Exercise3.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.example5 = exports.example4 = exports.example3 = exports.example2 = exports.example1 = void 0;
-exports.example1 = "Hello World!";
-exports.example2 = 42;
-exports.example3 = true;
-exports.example4 = Symbol();
-exports.example5 = 123n;
